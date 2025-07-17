c615fdfdc5005dd49f6f986100622c1ac3f06416
tsmay1 <againstonine@gmail.com>
2025-07-16 22:35:38 -0400

    small steps

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import os
+import shutil
+
 def main():
     print("Hello from static-site-generator!")
 
 
 if __name__ == "__main__":
     main()
+
+def delete_public_directory():
+    print("Deleting existing public directory.")
+    
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+        print("Public directory deleted successfully.")
+    else:
+        print("Public directory does not exist.")
+
+def create_public_directory():
+    print("Creating public directory.")
+    
+    if not os.path.exists("public"):
+        os.mkdir("public")
+        print("Public directory created successfully.")
+    else:
+        print("Public directory already exists.")
+
+def copy_static_to_public(static):
+
+    

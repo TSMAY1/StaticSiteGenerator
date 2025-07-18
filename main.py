9475329601e1f7e263ec71abf853e11d7ca3b5cf
tsmay1 <againstonine@gmail.com>
2025-07-17 22:46:13 -0400

    copy function complete

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import os
 import shutil
 
-def main():
-    print("Hello from static-site-generator!")
-
-
-if __name__ == "__main__":
-    main()
 
 def delete_public_directory():
     print("Deleting existing public directory.")
@@ -27,5 +21,32 @@ def create_public_directory():
         print("Public directory already exists.")
 
 def copy_static_to_public(static):
+    static_path = os.path.join("static", static)
+    public_path = os.path.join("public", static)
 
-    
+    for file in os.listdir(static_path):
+        full_file_path = os.path.join(static_path, file)
+        
+        if os.path.isfile(full_file_path):
+            destination_path = os.path.join(public_path, file)
+            print(f"Copying {full_file_path} to {destination_path}")
+            shutil.copy(full_file_path, destination_path)
+
+        if os.path.isdir(full_file_path):
+            destination_path = os.path.join(public_path, file)
+            if not os.path.exists(destination_path):
+                print(f"Creating directory {destination_path}")
+                os.mkdir(destination_path)
+
+            copy_static_to_public(file)
+
+def main():
+    print("Hello from static-site-generator!")
+    delete_public_directory()
+    create_public_directory()
+    copy_static_to_public("")
+
+
+
+if __name__ == "__main__":
+    main()
